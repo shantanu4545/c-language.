0aa5092b6d9e6ac76c4f2d2d4542c8e785a01f5c
shantanu4545 <shantanubhure77@gmail.com>
2025-11-06 21:39:50 +0530

    Add files via upload

---
+++ sum4num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, c, d;
+
+    printf("enter a \n");
+    scanf("%d", &a);
+
+    printf("enter b \n");
+    scanf("%d", &b);
+
+    printf("enter c \n");
+    scanf("%d",&c);
+
+    printf("enter d \n");
+    scanf("%d",&d);
+
+    printf("sum a + b + c + d is : %d \n",a+b+c+d);
+    return 0;
+}
