5bfa8088a8164686c087f3fc7eb4d51a532b1f7c
shantanu4545 <shantanubhure77@gmail.com>
2025-11-06 21:35:34 +0530

    Add files via upload

---
+++ 5sub.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+float main(){
+      float sub1,sub2,sub3,sub4,sub5,achievedmarks,percentage;
+      float totalmarks=500.0;
+
+      printf("Enter the marks subject 1:");
+      scanf("%d", &sub1);
+
+      printf("Enter the marks subject 2:");
+      scanf("%d", &sub2);
+      
+      printf("Enter the marks subject 3:");
+      scanf("%d", &sub3);
+      
+
+      printf("Enter the marks subject 4:");
+      scanf("%d", &sub4);
+      
+      printf("Enter the marks subject 5:");
+      scanf("%d", &sub5);
+      printf("total marks = %f\n",totalmarks);
+      achievedmarks=sub1+sub2+sub3+sub4+sub5;
+      printf("your achieved marks are %.2")
+
+
+}
