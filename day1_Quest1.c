053d83267beaee398d05815f6bcf0831423a440e
shantanu4545 <shantanubhure77@gmail.com>
2025-11-06 23:54:18 +0530

    Add files via upload

---
+++ day1_Quest1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+    printf("enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("even number");
+    else
+        printf("even number");
+
+    return 0;
+}
