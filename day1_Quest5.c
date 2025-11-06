58c4b9f465f5654d565f596fe38e80e2e468660b
shantanu4545 <shantanubhure77@gmail.com>
2025-11-06 23:56:19 +0530

    Add files via upload

---
+++ day1_Quest5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int year;
+    printf("Enter year: ");
+    scanf("%d",&year);
+
+    if((year % 400 == 0) ||(year % 4 == 0 && year % 100 !=0))
+        printf ("leap year. ");
+    else
+        printf("not a leap year. ");
+
+    return 0;
+}
