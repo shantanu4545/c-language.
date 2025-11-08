39dbfe3e97711b2fc35637dc45320bc9c96eaffd
shantanu4545 <shantanubhure77@gmail.com>
2025-11-08 23:02:16 +0530

    Add files via upload

---
+++ day1_Quest6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    printf("enter character: ");
+    scanf("%d",&ch);
+
+    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch =='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        printf ("vowel.");
+    else
+        printf("consonant.");
+    return 0;
+}
